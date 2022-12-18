754669a1970ee75b773503dc782d7b11a29983f2
afisarsy <afisarsy@gmail.com>
2022-12-18 23:38:15 +0700

    fix incorrect directories

--- handler/webHandler.js
+++ handler/webHandler.js
@@ -11,14 +11,14 @@ let locationRouter = require('../api/locationApi');
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/users', usersRouter);
 app.use('/devices', devicesRouter);
@@ -33,8 +33,8 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  console.log(req.app.get('env'));
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //console.log(req.app.get('env'));
 
   // render the error page
   res.status(err.status || 500);
